58e08b44ae6e76298e86d67c1673aba62d02a3f7
dannguyen <dan.nguyen@sweetsoft.vn>
2026-08-07 16:51:27 +0700

    style(lint): format python files with ruff

--- tests/test_ast_context_engine.py
+++ tests/test_ast_context_engine.py
@@ -5,7 +5,7 @@ from src.domain.ast_context_engine import ASTContextEngine
 
 def test_resolve_html_inline_event_scope():
     engine = ASTContextEngine()
-    line = '<button onclick="switchTab(\'profile\')">Tab</button>'
+    line = "<button onclick=\"switchTab('profile')\">Tab</button>"
     scope = engine.resolve_scope("index.html", 10, line)
     assert scope == "html-inline-event"
 
